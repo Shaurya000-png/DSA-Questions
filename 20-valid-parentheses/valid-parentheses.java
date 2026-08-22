1c9020688d5292f6fa5ad6370d0d9c3a5a6ecbc1
Shaurya Pratap Singh <shauryapratapsingh9415@gmail.com>
2026-08-22 14:25:24 +0530

    Time: 3 ms (86.03%) | Memory: 42.7 MB (99.11%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,22 +1,23 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack <Character> stack = new Stack<>();
+        Stack < Character > stack = new Stack<>();
 
-        for (char ch : s.toCharArray()) {
-            boolean isOpening = (ch == '(' || ch == '{' || ch == '[');
-            boolean isClosing = (ch == ')' || ch == '}' || ch == ']');
+        for ( char ch : s.toCharArray() ) {
+            boolean isOpen = ( ch == '(' || ch == '{' || ch == '[' );
+            boolean isClose = ( ch == ')' || ch == '}' || ch == ']');
 
-            if (isOpening) {
+            if ( isOpen ) {
                 stack.push(ch);
-            } else if (isClosing){
+            } else if ( isClose ) {
                 if (stack.isEmpty()) return false;
-
 
                 char top = stack.pop();
 
-                boolean isMismatch = ( top == '(' && ch != ')' ) || ( top == '{' && ch != '}' ) || ( top == '[' && ch != ']' );
+                boolean isMismatch = (( top == '(' && ch != ')') || ( top == '{' && ch != '}' ) || ( top == '[' && ch != ']'));
 
-                if (isMismatch) return false;
+                if ( isMismatch ) {
+                    return false;
+                }
             }
         }
         return stack.isEmpty();
